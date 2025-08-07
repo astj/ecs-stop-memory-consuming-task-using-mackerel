fc767bd919d79330055c7ef19e6c05047a3095ff
Asato Wakisaka <asato.wakisaka@gmail.com>
2025-08-07 21:54:55 +0900

    do not handle AWS_* in app

--- main.go
+++ main.go
@@ -17,8 +17,6 @@ type Config struct {
 	MackerelService string
 	MackerelRole    string
 	MackerelMetric  string
-	AWSProfile      string
-	AWSRegion       string
 	MackerelAPIKey  string
 }
 
@@ -29,7 +27,7 @@ func main() {
 	mackerelClient := mackerel.NewClient(c.MackerelAPIKey)
 	mackerelClient.Verbose = c.Verbose
 
-	awsConfig, err := config.LoadDefaultConfig(ctx, config.WithRegion(c.AWSRegion))
+	awsConfig, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		log.Fatalf("unable to load SDK config, %v", err)
 	}
@@ -60,8 +58,6 @@ func parseFlags() *Config {
 	flag.StringVar(&config.MackerelService, "mackerel-service", "", "Mackerel service name")
 	flag.StringVar(&config.MackerelRole, "mackerel-role", "", "Mackerel role name")
 	flag.StringVar(&config.MackerelMetric, "mackerel-metric", "", "Mackerel metric name for memory consumption")
-	flag.StringVar(&config.AWSProfile, "aws-profile", "", "AWS profile name")
-	flag.StringVar(&config.AWSRegion, "aws-region", "", "AWS region")
 	flag.StringVar(&config.MackerelAPIKey, "mackerel-api-key", "", "Mackerel API key")
 	flag.BoolVar(&config.Verbose, "verbose", false, "Enable verbose output")
 
@@ -83,14 +79,6 @@ func parseFlags() *Config {
 
 	if config.MackerelMetric == "" {
 		config.MackerelMetric = os.Getenv("MACKEREL_METRIC")
-	}
-
-	if config.AWSProfile == "" {
-		config.AWSProfile = os.Getenv("AWS_PROFILE")
-	}
-
-	if config.AWSRegion == "" {
-		config.AWSRegion = os.Getenv("AWS_REGION")
 	}
 
 	if config.MackerelAPIKey == "" {
